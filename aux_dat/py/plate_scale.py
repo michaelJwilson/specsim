0d3b918acb7a2638812516ad5a55beb1102dd314
Michael J. Wilson <michaeljwilson@lbl.gov>
2019-01-05 11:04:38 -0800

    Added script to calculate PFS plate scale, plot and read in config.

---
+++ aux_dat/py/plate_scale.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pylab as pl
+
+
+dat     = np.loadtxt('focalplane/platescale.txt') 
+wdat    = dat[:,0]
+
+## Assume MPS == SPS 
+pscale  = dat[:,6]
+
+## Find radius where DESI ratio to centre is 10%.
+index   = np.where(np.abs(pscale / pscale[0] - 1.1).min() == np.abs(pscale / pscale[0] - 1.1))
+
+##  Normalise to platescale to 90.9 at origin.
+pscale *= 90.9 / pscale[0]
+wdat   *= 224. / wdat[index]
+
+print(pscale[index] / pscale[0])
+
+##  Assume radial and azimuthal platescale are the same.
+output  = np.c_[wdat[wdat <= 225.], pscale[wdat <= 225.], pscale[wdat <= 225.]]
+
+np.savetxt('platescale-pfs.txt', output, header='# Radius [mm], Radial platescale [um/arcsec], Azimuthal platescale [um/arcsec]', fmt='%.6lf')
+
+pl.plot(wdat[wdat <= 225.], pscale[wdat <= 225.])
+pl.xlabel(r'$Radius \ [mm]$')
+pl.ylabel(r'Plate scale $[\mu \rm{m/arcsec}]$')
+pl.savefig('plots/platescale.pdf')
+
